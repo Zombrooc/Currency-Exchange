38ccdb1bb0c51473b90500e44f8dd7512da73fec
Elian Valdez <elian.valdez09@gmail.com>
2021-08-27 10:19:38 -0300

    Some changes

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 const usdInput = document.getElementById("USD");
 const brlInput = document.getElementById("BRL");
-const convertButton = document.getElementById("convertButton");
+//const convertButton = document.getElementById("convertButton");
 
-convertButton.addEventListener("click", () => {
-  fetch("https://api.coinbase.com/v2/exchange-rates")
-    .then((res) => res.json())
+const replaceComma = (string) => {
+  return string.replace(',', '.');
+}
+
+usdInput.addEventListener("input", (event) => {
+
+  let targetValue = replaceComma(event.target.value);
+  
+  fetch("https://api.coinbase.com/v2/exchange-rates?currency=USD")
+    .then((response) => {
+      return response.json();
+    })
     .then((data) => {
-      const usdValue = usdInput.value;
-
-      brlInput.value = (usdValue * data.data.rates.BRL).toLocaleString(
+      brlInput.value = (targetValue * data.data.rates.BRL).toLocaleString(
         "pt-br",
         { style: "currency", currency: "BRL" }
+      );
+    });
+});
+
+brlInput.addEventListener("input", (event) => {
+
+  let targetValue = replaceComma(event.target.value);
+
+  fetch("https://api.coinbase.com/v2/exchange-rates?currency=USD")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      usdInput.value = (targetValue / data.data.rates.BRL).toLocaleString(
+        "us",
+        { style: "currency", currency: "USD" }
       );
     });
 });
